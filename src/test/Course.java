e2bdf19e3858301deab419730438f3e9f09b3b89
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2022-12-24 23:42:34 -0800

    list3 list

--- src/test/Course.java
+++ src/test/Course.java
@@ -1,8 +1,11 @@
 package test;
 
+import java.util.ArrayList;
+
 public class Course {
 	private int curseID;
 	Mark marObj = new Mark();
+	ArrayList<Mark> techList = new ArrayList<Mark>();
 	
 	public int getCurseID() {
 	    return curseID;

ecd50192d05798ea9e677754ce85df2341ecc552
AbdulhamidNasser <48241945+AbdulhamidNasser@users.noreply.github.com>
2022-12-27 23:40:34 -0800

    Search in file

--- src/test/MainStack.java
+++ src/test/MainStack.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import java.util.Stack;
 import java.util.List;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -23,12 +24,36 @@ public class MainStack {
 			FileWriter fw = new FileWriter(fil);
 			for(String x : sck) {
 	    		fw.write(x);
+	    		fw.write("-");
 	    	}
 			fw.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    	System.out.println("Enter word need to search :");
+    	String serachWord = sc.next(); 
+    	int counter = 0;
+    	try {
+    		Scanner ns = new Scanner(fil);
+    		while (ns.hasNext()) {
+    			String[] content = ns.nextLine().split("-");
+    			for(int i = 0; i < content.length; i++) {
+    				
+    				if(serachWord.equalsIgnoreCase(content[i])) {
+    					counter++;
+    					System.out.println(" the word : " + content[i]);
+    				}
+    			}
+    			
+    		}
+    		System.out.println("this word search is in the file :"+counter);
+    		ns.close();
+    	} catch (FileNotFoundException e) {
+    		e.printStackTrace();
+    	}
+    	
+    	
     	
     	
     	
